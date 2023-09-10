fc08a04ce230bde51b85913b43852d711afdfb27
Naveen <practialengineers05@gmail.com>
2023-09-10 22:27:22 +0530

    Project Mapstruct

---
+++ src/main/java/org/amba/app/Mapper/ProjectMapper.java
@@ -0,0 +1,23 @@
+package org.amba.app.Mapper;
+
+import org.amba.app.Dto.ProjectDto;
+import org.amba.app.Entity.Project;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProjectMapper {
+
+    ProjectMapper INSTANCE = Mappers.getMapper(ProjectMapper.class );
+
+
+    @Mapping(source = "imageData", target = "image")
+    ProjectDto ProjectToProjectSto(Project p);
+
+
+    List<ProjectDto> ProjectsToProjectsDto(List<Project> p);
+
+}
