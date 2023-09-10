ec8741ea74397125c56b552fcf337f0fcd74792b
Naveen <practialengineers05@gmail.com>
2023-09-10 22:26:36 +0530

    cors fix

--- src/main/java/org/amba/app/Application.java
+++ src/main/java/org/amba/app/Application.java
@@ -3,12 +3,26 @@ package org.amba.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String agrs[]){
         SpringApplication.run(Application.class,agrs);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
