cc496c2e1b83ab182d5efd4d543e697ab25b98ba
engineerscodes <practicalengineers05@gmail.com>
2023-09-17 22:10:32 +0530

    New DTO

---
+++ src/main/java/org/amba/app/Dto/ProjectNoImageDto.java
@@ -0,0 +1,19 @@
+package org.amba.app.Dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.util.UUID;
+
+
+@Data
+public class ProjectNoImageDto {
+
+    private UUID id;
+
+    private String projectName;
+
+
+    private byte[] image;
+
+}
