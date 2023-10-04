e2b1de254019d56b66f7a257d3642bfb96983718
engineerscodes <practicalengineers05@gmail.com>
2023-10-04 22:44:20 +0530

    User controller

---
+++ src/main/java/org/amba/app/Controllers/UserController.java
@@ -0,0 +1,14 @@
+package org.amba.app.Controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+// https://www.youtube.com/watch?v=KxqlJblhzfI
+@RequestMapping("user")
+public class UserController {
+
+
+
+
+}
