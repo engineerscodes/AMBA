2c160ce11b2a15913cd4542be27a55ab35180579
engineerscodes <practicalengineers05@gmail.com>
2023-10-07 19:14:04 +0530

    New DTO

--- src/main/java/org/amba/app/Dto/NewUserDto.java
+++ src/main/java/org/amba/app/Dto/NewUserDto.java
@@ -3,22 +3,23 @@ package org.amba.app.Dto;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewUserDto {
-    private UUID userId;
-
+    private String name;
 
     @Email(message = "Please provide a valid email address")
-    @Column(unique = true, nullable = false)
     private String email;
 
-
-    // Hashed password is stored
     private String password;
+
 }
