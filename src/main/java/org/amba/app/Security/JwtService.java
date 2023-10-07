885dcd47fbffa527b5298f0ab2846a34487f0305
engineerscodes <practicalengineers05@gmail.com>
2023-10-07 19:13:55 +0530

    JwtService class

---
+++ src/main/java/org/amba/app/Security/JwtService.java
@@ -0,0 +1,77 @@
+package org.amba.app.Security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+
+    @Value("${jwt.key.private}")
+    private String PRIVATE_KEY ;
+
+    private static final Long ONE_YEAR =  (1000L * 60 * 60 * 24 * 30) ; // if 30 days
+
+
+    public SecretKey getPrivateKey(){
+        byte[] privateKeyBytes = Decoders.BASE64.decode(PRIVATE_KEY);
+        SecretKey key = Keys.hmacShaKeyFor(privateKeyBytes);
+        return Keys.hmacShaKeyFor(privateKeyBytes);
+    }
+
+
+    public <T> T getClaims(String token, Function <Claims , T> claimResolver ){
+        final  Claims claims = getAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public String getUsername(String jwtToken){
+        return getClaims(jwtToken,Claims::getSubject);
+    }
+
+    public String generateToken(UserDetails user){
+        return generateToken(new HashMap<>(),user);
+    }
+
+
+    public boolean isValidToken(String token,UserDetails user){
+       final String UserName = getUsername(token); // email is the userName
+       return (UserName.equals(user.getUsername()) && !isExpiredToken(token));
+    }
+
+    public boolean isExpiredToken(String token ){
+        return getExpirationDate(token).before(new Date());
+    }
+
+    public Date getExpirationDate(String token){
+        return getClaims(token,Claims::getExpiration);
+    }
+
+    public String generateToken(Map<String , Object> newClaims, UserDetails user){
+         return Jwts.builder().claims(newClaims).subject(user.getUsername())
+                 .issuedAt(new Date(System.currentTimeMillis()))
+                 .expiration(new Date(System.currentTimeMillis() + ONE_YEAR))
+                 .signWith(getPrivateKey(), Jwts.SIG.HS256)  // https://github.com/jwtk/jjwt#signaturealgorithm-override
+                 .compact();
+    }
+
+    private Claims getAllClaims(String token){
+        // throw JwtException - runtime exception unchecked
+        // https://github.com/jwtk/jjwt#constant-parsing-key
+        return Jwts.parser().verifyWith(getPrivateKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+}
---
+++ src/main/java/org/amba/app/Service/AuthenticationService.java
@@ -0,0 +1,65 @@
+package org.amba.app.Service;
+
+
+import org.amba.app.Controllers.QuestionController;
+import org.amba.app.Dto.AuthenticationResponseDTO;
+import org.amba.app.Dto.LoginUserDTO;
+import org.amba.app.Dto.NewUserDto;
+import org.amba.app.Entity.User;
+import org.amba.app.Repo.UserRepo;
+import org.amba.app.Security.JwtService;
+import org.amba.app.Util.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    Logger log = LoggerFactory.getLogger(AuthenticationService.class);
+
+
+    public AuthenticationResponseDTO signUp(NewUserDto newUserDto) {
+        User user = User.builder()
+                .email(newUserDto.getEmail())
+                .name(newUserDto.getName())
+                .password(passwordEncoder.encode(newUserDto.getPassword()))
+                .role(Role.USER)
+                .build();
+        String jwtToken = jwtService.generateToken(user);
+        userRepo.save(user);
+        log.info("New User created with Email ID : {}",newUserDto.getEmail());
+        return AuthenticationResponseDTO.builder()
+                .token(jwtToken).build();
+
+    }
+
+    public AuthenticationResponseDTO login(LoginUserDTO loginUserDTO) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginUserDTO.getEmail(),
+                loginUserDTO.getPassword()
+        ));
+
+        User user = userRepo.findByEmail(loginUserDTO.getEmail())
+                .orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponseDTO.builder()
+                .token(jwtToken).build();
+    }
+}
