e7136ca6a9f11f70def22f503395615cb65c802d
engineerscodes <practicalengineers05@gmail.com>
2023-10-07 19:16:01 +0530

    SecurityConfiguration

---
+++ src/main/java/org/amba/app/Security/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package org.amba.app.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+
+    @Autowired
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private final AuthenticationProvider authenticationProvider;
+
+    // @Autowired// private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http.csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(auth ->
+                       auth.requestMatchers("/auth/**").permitAll().anyRequest().authenticated()
+
+               ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .authenticationProvider(authenticationProvider)
+               .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+              /* .logout(logout ->
+                       logout.logoutUrl("/auth/logout")
+                               .addLogoutHandler(logoutHandler)
+                               .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+               ) */
+       ;
+
+       return http.build();
+    }
+
+}
