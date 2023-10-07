721aba9d10109e6dec42aac7429dc0c73f90b804
engineerscodes <practicalengineers05@gmail.com>
2023-10-07 19:11:42 +0530

    auth contoller

---
+++ src/main/java/org/amba/app/Controllers/AuthenticationController.java
@@ -0,0 +1,35 @@
+package org.amba.app.Controllers;
+
+import org.amba.app.Dto.AuthenticationResponseDTO;
+import org.amba.app.Dto.LoginUserDTO;
+import org.amba.app.Dto.NewUserDto;
+import org.amba.app.Service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/signUp")
+    private ResponseEntity<AuthenticationResponseDTO> signUp(@RequestBody NewUserDto newUserDto){
+       return ResponseEntity.ok(authenticationService.signUp(newUserDto));
+    }
+
+    @PostMapping("/login")
+    private ResponseEntity<AuthenticationResponseDTO> login(@RequestBody LoginUserDTO loginUserDTO){
+        return ResponseEntity.ok(authenticationService.login(loginUserDTO));
+    }
+
+
+}
