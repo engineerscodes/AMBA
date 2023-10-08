b878a0630c5236de89c63a02a7fd07e6356f94e8
engineerscodes <practicalengineers05@gmail.com>
2023-10-08 19:48:44 +0530

    updated Readme

--- src/main/java/org/amba/app/Security/SecurityConfiguration.java
+++ src/main/java/org/amba/app/Security/SecurityConfiguration.java
@@ -32,8 +32,8 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
        http.csrf(AbstractHttpConfigurer::disable)
                .authorizeHttpRequests(auth ->
-                       auth.requestMatchers("/auth/**").permitAll().anyRequest().authenticated()
-
+                       auth.requestMatchers("**").permitAll()
+                               .anyRequest().authenticated()
                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                .authenticationProvider(authenticationProvider)
                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
