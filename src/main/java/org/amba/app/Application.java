1da6b51ee5163d279e74d3e1274c4625e5123269
engineerscodes <practicalengineers05@gmail.com>
2023-10-17 00:14:53 +0530

    CORS SETUP

--- src/main/java/org/amba/app/Application.java
+++ src/main/java/org/amba/app/Application.java
@@ -4,8 +4,13 @@ package org.amba.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -15,7 +20,7 @@ public class Application {
     }
 
 
-    @Bean
+   /* @Bean
     public WebMvcConfigurer corsConfigure() {
         return new WebMvcConfigurer() {
             @Override
@@ -23,6 +28,19 @@ public class Application {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
+    }*/
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://127.0.0.1:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
