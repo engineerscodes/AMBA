87a52018502628cbff102a6502847c2f9edf535e
engineerscodes <practicalengineers05@gmail.com>
2023-10-20 23:18:50 +0530

    Updated query

--- src/main/java/org/amba/app/Repo/UserRepo.java
+++ src/main/java/org/amba/app/Repo/UserRepo.java
@@ -1,8 +1,11 @@
 package org.amba.app.Repo;
 
+import org.amba.app.Dto.UserDTO;
 import org.amba.app.Entity.User;
+import org.amba.app.Util.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +13,7 @@ public interface UserRepo extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByUserIdAndEmail(UUID uuid,String email);
+
+    List<UserDTO> findByRole(Role role);
 }
