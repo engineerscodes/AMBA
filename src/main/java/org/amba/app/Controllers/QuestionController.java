a711801477ac23295477a3cef713a4edd0fc130b
engineerscodes <practicalengineers05@gmail.com>
2023-10-24 21:42:44 +0530

    question submit added

--- src/main/java/org/amba/app/Controllers/QuestionController.java
+++ src/main/java/org/amba/app/Controllers/QuestionController.java
@@ -5,9 +5,13 @@ import org.amba.app.Dto.QuestionDTO;
 import org.amba.app.Entity.Project;
 import org.amba.app.Entity.Question;
 import org.amba.app.Entity.Type;
+import org.amba.app.Entity.User;
 import org.amba.app.Repo.ProjectRepo;
+import org.amba.app.Repo.QuestionRepo;
 import org.amba.app.Repo.TypeRepo;
+import org.amba.app.Repo.UserRepo;
 import org.amba.app.Service.QuestionService;
+import org.amba.app.Service.UserService;
 import org.amba.app.Util.Options;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -40,11 +46,14 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
+    @Autowired
+    UserService userService;
+
 
     @GetMapping("/{id}/all")
     private ResponseEntity<List<QuestionDTO>> getAllQuestion(@PathVariable("id") String projectId,
                                                              @RequestParam(defaultValue = "0") int page,
-                                                             @RequestParam(defaultValue = "10") int size){
+                                                             @RequestParam(defaultValue = "5") int size){
 
         Project p = questionService.checkValidProject(projectId);
         if(p == null) return  ResponseEntity.badRequest().body(null);
@@ -54,6 +63,34 @@ public class QuestionController {
         List<QuestionDTO> questionList = questionService.getAllQuestion(p,pageable);
 
         return ResponseEntity.ok().body(questionList);
+    }
+    @PutMapping("/{id}/submit")
+    private  ResponseEntity<Object> submitAnswer(@PathVariable("id") String QuestionID ,
+                                                 @RequestParam(name = "answerId") int answerIndex){
+       try {
+           Optional<Question> questionOp = questionService.findQuestionByUuid(UUID.fromString(QuestionID));
+           if(questionOp.isEmpty()) return  ResponseEntity.badRequest().body("No Question Uuid Found");
+           Question question = questionOp.get();
+           if(answerIndex>=question.getOptions().size()) return ResponseEntity.badRequest().body("AnswerIndex greater than no of options");
+           if(question.getAnswerID()!=answerIndex) return ResponseEntity.badRequest().body("Not correct answer");
+           Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+           if(authentication.isAuthenticated()){
+               User user = (User) authentication.getPrincipal();
+               List<UUID> list = user.getQuestionsCompleted()==null? new ArrayList<UUID>():user.getQuestionsCompleted();
+               if(list.parallelStream().anyMatch(e-> e.equals(question.getQuestionID()))){
+                   return ResponseEntity.badRequest().body("Question is already submitted");
+               }
+               list.add(question.getQuestionID());
+               user.setQuestionsCompleted(list);
+               user = userService.saveUser(user);
+               log.info("User {} submitted answer {} for question {}",user.getUserId(),answerIndex,QuestionID);
+               return ResponseEntity.ok(user.getQuestionsCompleted());
+           }else
+               return ResponseEntity.badRequest().body("No User Found Authenticated");
+       }catch (Exception e){
+           System.out.println(e);
+           return ResponseEntity.badRequest().body("Error in saving Project check Project UUID or Saving Question Problem");
+       }
     }
 
     @PostMapping(value = "/new", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
