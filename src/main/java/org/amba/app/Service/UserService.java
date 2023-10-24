8b317de1e564233b0a339b5e8037eec766271c0c
engineerscodes <practicalengineers05@gmail.com>
2023-10-24 21:41:17 +0530

    dated save user

--- src/main/java/org/amba/app/Service/UserService.java
+++ src/main/java/org/amba/app/Service/UserService.java
@@ -1,8 +1,20 @@
 package org.amba.app.Service;
 
 
+import org.amba.app.Entity.User;
+import org.amba.app.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepo userRepo;
+
+
+    public User saveUser(User user){
+        return userRepo.saveAndFlush(user);
+    }
+
 }
