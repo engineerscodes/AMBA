49dc45c771f961756bef0fa6f9e68c9966fafbbf
engineerscodes <practicalengineers05@gmail.com>
2023-10-24 14:30:14 +0530

    Fixed auto increment value

--- src/main/java/org/amba/app/Entity/Question.java
+++ src/main/java/org/amba/app/Entity/Question.java
@@ -1,14 +1,14 @@
 package org.amba.app.Entity;
 
+
 import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.amba.app.Util.Options;
-import org.hibernate.annotations.JdbcType;
 import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.Type;
 import org.hibernate.type.SqlTypes;
 
 
@@ -29,7 +29,7 @@ public class Question {
     private UUID questionID;
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "question_number", unique = true, nullable = false, insertable = false, updatable = false)
+    @Column(name = "question_number",columnDefinition = "SERIAL", unique = true, nullable = false, insertable = false, updatable = false)
     private BigInteger questionNumber;
 
     /**
