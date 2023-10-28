3c840d9dd746b24fccf6d2ec04764822af76b61b
engineerscodes <practicalengineers05@gmail.com>
2023-10-28 21:24:59 +0530

    Enable Scheduling

--- src/main/java/org/amba/app/Application.java
+++ src/main/java/org/amba/app/Application.java
@@ -4,6 +4,7 @@ package org.amba.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String agrs[]){
