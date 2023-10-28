dfc8303e1c736d88a77203904ab0cc63428c41b0
engineerscodes <practicalengineers05@gmail.com>
2023-10-28 21:24:36 +0530

    projection on find all

--- src/main/java/org/amba/app/Repo/ProjectRepo.java
+++ src/main/java/org/amba/app/Repo/ProjectRepo.java
@@ -1,12 +1,19 @@
 package org.amba.app.Repo;
 
 
+import org.amba.app.Crons.DTO.ProjectCronDTO;
 import org.amba.app.Entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProjectRepo extends JpaRepository<Project, UUID> {
+
+    @Query("select new org.amba.app.Crons.DTO.ProjectCronDTO(p.id , p.projectName , p.type.type) from Project p")
+    List<ProjectCronDTO> findAllProjects();
+
 }
