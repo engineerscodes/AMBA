d54347c687b3fcd7ffc5a636cbd0523cb025bd90
engineerscodes <practicalengineers05@gmail.com>
2023-10-28 21:24:46 +0530

    report mapper

---
+++ src/main/java/org/amba/app/Mapper/AdminReportMapper.java
@@ -0,0 +1,36 @@
+package org.amba.app.Mapper;
+
+
+import org.amba.app.Crons.DTO.QuestionCount;
+import org.amba.app.Crons.DTO.UserCronDTOProjection;
+import org.amba.app.Dto.ReportDTO;
+import org.amba.app.Entity.Report;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface AdminReportMapper {
+
+    AdminReportMapper INSTANCE = Mappers.getMapper(AdminReportMapper.class);
+
+    @Mapping(source = "question.questionCount",target ="score")
+    @Mapping(source = "question.projectName",target ="project")
+    @Mapping(source = "question.type",target ="type")
+    @Mapping(source = "user.role",target ="role")
+    @Mapping(source = "user.email",target = "email")
+    @Mapping(source = "user.questionsCompleted",target = "questionCompleted")
+    @Mapping(source = "dateTime",target = "reportDate")
+    ReportDTO from(QuestionCount question, UserCronDTOProjection user, List<BigInteger> questionNumber, LocalDateTime dateTime,long totalQuestions);
+
+
+    @Mapping(source = "reportDTO.reportDate",target = "reportDateTime")
+    @Mapping(source = "reportDTO.project",target = "Project")
+    @Mapping(source = "reportDTO.questionCompleted",target = "questionsCompleted")
+    Report fromDTO(ReportDTO reportDTO);
+
+}
