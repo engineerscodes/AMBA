0ce4e72637211b1be24799051a19ecddd9fc4d57
engineerscodes <practicalengineers05@gmail.com>
2023-10-28 21:21:43 +0530

    find all users

--- src/main/java/org/amba/app/Repo/UserRepo.java
+++ src/main/java/org/amba/app/Repo/UserRepo.java
@@ -1,9 +1,11 @@
 package org.amba.app.Repo;
 
+import org.amba.app.Crons.DTO.UserCronDTOProjection;
 import org.amba.app.Dto.UserDTO;
 import org.amba.app.Entity.User;
 import org.amba.app.Util.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +18,7 @@ public interface UserRepo extends JpaRepository<User, UUID> {
     Optional<User> findByUserIdAndEmail(UUID uuid,String email);
 
     List<UserDTO> findByRole(Role role);
+
+    @Query("select new org.amba.app.Crons.DTO.UserCronDTOProjection(u.email,u.questionsCompleted,u.role) from User u")
+    List<UserCronDTOProjection> findAllProjection();
 }
