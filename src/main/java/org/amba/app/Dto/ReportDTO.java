778ad16f242847de491ffbcbca8ac1a952ead3f5
engineerscodes <practicalengineers05@gmail.com>
2023-10-28 21:23:15 +0530

    report DTO

---
+++ src/main/java/org/amba/app/Dto/ReportDTO.java
@@ -0,0 +1,49 @@
+package org.amba.app.Dto;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportDTO {
+
+
+    @Email
+    String email;
+
+    @NotNull
+    String project;
+
+    @NotNull
+    String type;
+
+    @NotNull
+    List<String> questionCompleted;
+
+    @NotNull
+    List<String>  questionNumber;
+
+    @NotNull
+    long score;
+
+    @NotNull
+    String role;
+
+    @NotNull
+    LocalDateTime reportDate;
+
+    @NotNull
+    long totalQuestions;
+}
