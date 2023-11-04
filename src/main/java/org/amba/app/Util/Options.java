b3c947dfc76771096fd56695f74eb08e3c6cb49f
engineerscodes <practicalengineers05@gmail.com>
2023-11-04 17:21:22 +0530

    update options

--- src/main/java/org/amba/app/Util/Options.java
+++ src/main/java/org/amba/app/Util/Options.java
@@ -1,10 +1,15 @@
 package org.amba.app.Util;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Options {
 
     String answer;
