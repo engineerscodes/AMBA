f16bda7b9c1e2bf8821aa95b7254c996fd0c7422
engineerscodes <practicalengineers05@gmail.com>
2024-02-05 23:26:53 +0530

    logic for new sheet

--- src/main/java/org/amba/app/Service/ReportService.java
+++ src/main/java/org/amba/app/Service/ReportService.java
@@ -3,13 +3,17 @@ package org.amba.app.Service;
 import org.amba.app.Dto.ReportDTO;
 import org.amba.app.Util.Doc;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 
 
@@ -19,11 +23,14 @@ public class ReportService {
     List<String> columns = List.of("EMAIL", "PROJECT", "PROJECT TYPE",
             "QUESTION ANSWERED","SCORE","TOTAL QUESTION AVAILABLE","USER ROLE","REPORT DATE TIME");
 
+    List<String> questionCompletedColumns = List.of("Email","PROJECT","PROJECT TYPE","QUESTION IMAGE","QUESTION TEXT","ANSWER","REPORT DATE TIME");
+
     public Doc generateReport(){
 
 
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Student data");
+        XSSFSheet questionCompleted = workbook.createSheet("Question Completed Report");
 
         CreationHelper createHelper = workbook.getCreationHelper();
 
@@ -36,13 +43,21 @@ public class ReportService {
         headerCellStyle.setFont(headerFont);
 
         Row headerRow = sheet.createRow(0);
+        Row headerRowQ = questionCompleted.createRow(0);
 
         for(int i = 0; i < columns.size(); i++) {
             Cell cell = headerRow.createCell(i);
             cell.setCellValue(columns.get(i));
             cell.setCellStyle(headerCellStyle);
         }
-        return Doc.builder().workbook(workbook).sheet(sheet).createHelper(createHelper).build();
+
+        for(int i = 0; i < questionCompletedColumns.size(); i++) {
+            Cell cell = headerRowQ.createCell(i);
+            cell.setCellValue(questionCompletedColumns.get(i));
+            cell.setCellStyle(headerCellStyle);
+        }
+
+        return Doc.builder().workbook(workbook).sheet(sheet).questionCompletedSheet(questionCompleted).createHelper(createHelper).build();
     }
 
     public void addRow( XSSFWorkbook workbook, XSSFSheet sheet,CreationHelper createHelper ,ReportDTO reportDTO){
@@ -63,6 +78,41 @@ public class ReportService {
         date.setCellValue(reportDTO.getReportDate());
         date.setCellStyle(dateCellStyle);
 
+    }
+
+    public void addRowQuestionSheet(XSSFWorkbook workbook, XSSFSheet sheet, CreationHelper createHelper , byte[] image, String question, String answer, String email
+    , String Project, String Project_type, BigInteger questionNo){
+
+        CellStyle dateCellStyle = workbook.createCellStyle();
+        dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-MM-yyyy"));
+
+        int questionImg = workbook.addPicture(image, Workbook.PICTURE_TYPE_PNG);
+        XSSFDrawing drawing = (XSSFDrawing) sheet.createDrawingPatriarch();
+        XSSFClientAnchor questionImgAnchor = new XSSFClientAnchor();
+
+
+        int lastRowNum = sheet.getLastRowNum();
+        Row row = sheet.createRow(++lastRowNum);
+
+
+        row.createCell(0).setCellValue(email);
+        row.createCell(1).setCellValue(Project);
+        row.createCell(2).setCellValue(Project_type);
+        row.createCell(3).setCellValue("Question No : "+questionNo);
+        row.createCell(4).setCellValue(question);
+        row.createCell(5).setCellValue(answer);
+        Cell date = row.createCell(6);
+        date.setCellValue(LocalDateTime.now());
+        date.setCellStyle(dateCellStyle);
+
+        questionImgAnchor.setCol1(3);
+        questionImgAnchor.setCol2(4);
+        questionImgAnchor.setRow1(lastRowNum);
+        questionImgAnchor.setRow2(lastRowNum+1);
+        drawing.createPicture(questionImgAnchor, questionImg);
+        for (int i=0;i<sheet.getRow(0).getLastCellNum();i++){
+            sheet.autoSizeColumn(i);
+        }
     }
 
     public void save( XSSFWorkbook workbook, XSSFSheet sheet) throws IOException {
